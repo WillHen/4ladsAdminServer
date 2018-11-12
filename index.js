fcd55a6e960cc1393fa97f3e73fa8c5496d6f122
William Henshaw <williamhenshaw@macbook.home>
2018-11-12 17:40:23 -0600

    Add get player by id

--- index.js
+++ index.js
@@ -4,79 +4,104 @@ const { Pool, Client } = require("pg");
 const Hapi = require("hapi");
 
 const pool = new Pool({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.FOUR_LADS_DB,
-	port: process.env.DB_PORT
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.FOUR_LADS_DB,
+    port: process.env.DB_PORT
 });
 
 pool.connect((err, client, done) => {
-	if (err) throw err;
+    if (err) throw err;
 });
 
 // Create a server with a host and port
 const server = Hapi.server({
-	host: "localhost",
-	port: 8000
+    host: "localhost",
+    port: 8000
 });
 
 const start = async () => {
-	try {
-		await server.start();
-	} catch (err) {
-		console.log(err);
-		process.exit(1);
-	}
+    try {
+        await server.start();
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
 };
 
 const getCompetitions = () => {
-	return new Promise((resolve, reject) => {
-		let query = {
-			name: "fetch-competitions",
-			text: "SELECT * FROM competitions"
-		};
-		pool.query(query, (err, res) => {
-			if (err) {
-				reject(err.stack);
-			} else {
-				resolve(res.rows);
-			}
-		});
-	});
+    return new Promise((resolve, reject) => {
+        let query = {
+            name: "fetch-competitions",
+            text: "SELECT * FROM competitions"
+        };
+        pool.query(query, (err, res) => {
+            if (err) {
+                reject(err.stack);
+            } else {
+                resolve(res.rows);
+            }
+        });
+    });
 };
 
 const getPlayers = () => {
-	return new Promise((resolve, reject) => {
-		let query = {
-			name: "fetch-players",
-			text: "SELECT * FROM players"
-		};
-		pool.query(query, (err, res) => {
-			if (err) {
-				reject(err.stack);
-			} else {
-				resolve(res.rows);
-			}
-		});
-	});
+    return new Promise((resolve, reject) => {
+        let query = {
+            name: "fetch-players",
+            text: "SELECT * FROM players"
+        };
+        pool.query(query, (err, res) => {
+            if (err) {
+                reject(err.stack);
+            } else {
+                resolve(res.rows);
+            }
+        });
+    });
+};
+
+const getPlayerById = id => {
+    return new Promise((resolve, reject) => {
+        let query = {
+            name: "fetch-player-by-id",
+            text: "SELECT * FROM players WHERE player_id =" + id
+        };
+        pool.query(query, (err, res) => {
+            if (err) {
+                reject(err.stack);
+            } else {
+                resolve(res.rows);
+            }
+        });
+    });
 };
 
 server.route({
-	method: "GET",
-	path: "/competitions",
-	handler: async (request, h) => {
-		let competitions = await getCompetitions();
-		return competitions;
-	}
+    method: "GET",
+    path: "/competitions",
+    handler: async (request, h) => {
+        let competitions = await getCompetitions();
+        return competitions;
+    }
 });
 
 server.route({
-	method: "GET",
-	path: "/players",
-	handler: async (request, h) => {
-		let players = await getCompetitions();
-		return competitions;
-	}
+    method: "GET",
+    path: "/players",
+    handler: async (request, h) => {
+        let players = await getPlayers();
+        return players;
+    }
+});
+
+server.route({
+    method: "GET",
+    path: "/getPlayer/{id?}",
+    handler: async (request, h) => {
+        let player = await getPlayerById(request.params.id);
+        return player;
+    }
 });
 
 start();
